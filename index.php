b24d08c9ab26c47bc508cd2355e63b48ed02fac4
leonardo748 <leonardo.ocs@outlook.com>
2018-05-22 14:13:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 require_once "login.php";
 
-$email = "";
-$senha = "";
+$email = "teste@teste.com";
+$senha = "teste";
 
 $login = new Facebook();
 $login->caminho = "cookies.txt";
 
-//verifica se ja está logado, "!" informando caso não esteja ele realiza o login
-if(!$login->estouLogado){
-  $login->entrar($email, $senha, false);
-}
+//realiza o login
+$login->entrar($email, $senha, false);
+
 //abre uma pagina do facebook
 echo $login->abrirPagina("https://www.facebook.com");
